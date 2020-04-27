e0a32f4ec1e9268683c1f50bae19aed13031b4b4
Glyn Normington <normingtong@vmware.com>
2020-04-27 14:37:00 +0100

    Single digits
    Following https://compilers.iecc.com/crenshaw/ as closely as possible.

---
+++ pkg/expr/parser.go
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2020 Go YAML Path Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package expr
+
+import (
+	"fmt"
+	"strings"
+)
+
+type lexeme string
+
+const (
+	add          lexeme = "+"
+	multiply     lexeme = "*"
+	openBracket  lexeme = "("
+	closeBracket lexeme = ")"
+	eof          lexeme = "."
+)
+
+/*
+   node represents a node of an expression parse tree. Each node is labelled with a lexeme.
+
+   Terminal nodes have an integer lexeme.
+
+   Non-terminal nodes represent an addition, multiplication, or bracketed expression.
+
+   The following examples illustrate the approach.
+
+   The basic expressions `1+2` is represented as the following parse tree:
+
+               +
+              / \
+             1   2
+
+   The expression `1 * 2 + 3` is represented as the parse tree:
+
+                               +
+                             /    \
+                            *      3
+                           / \
+                          1   2
+
+   The filter expression `(1+2)*3` is represented as the parse tree:
+
+                           *
+                        /     \
+					  ( )      3
+					   |
+					   +
+				     /   \
+				    1     2
+*/
+type node struct {
+	lexeme   lexeme
+	children []*node
+}
+
+func newNode(lexemes []lexeme) *node {
+	return newParser(lexemes).parse()
+}
+
+func (n *node) String() string {
+	return "---\n" + n.indentedString(0) + "\n---\n"
+}
+
+func (n *node) indentedString(indent int) string {
+	i := strings.Repeat("    ", indent)
+	s := n.lexeme
+	c := ""
+	for _, child := range n.children {
+		c = c + "\n" + child.indentedString(indent+1)
+	}
+	return fmt.Sprintf("%s%s%s", i, s, c)
+}
+
+// pstateFn represents the state of the parser as a function that returns the next state.
+// A nil pstateFn indicates lexing is complete.
+type pstateFn func(*parser) pstateFn
+
+// parser holds the state of the filter expression parser.
+// based on https://compilers.iecc.com/crenshaw/
+type parser struct {
+	input []lexeme // the lexemes being scanned
+	pos   int      // current position in the input
+	stack []*node  // parser stack
+	tree  *node    // parse tree, equivalent of D0
+}
+
+// lex creates a new scanner for the input string.
+func newParser(input []lexeme) *parser {
+	l := &parser{
+		input: input,
+		stack: make([]*node, 0),
+	}
+	return l
+}
+
+// push pushes a node on the stack
+func (p *parser) push(node *node) {
+	p.stack = append(p.stack, node)
+}
+
+// pop pops a node from the stack. If the stack is empty, panics.
+func (p *parser) pop() *node {
+	if len(p.stack) == 0 {
+		panic("parser stack underflow")
+	}
+	index := len(p.stack) - 1
+	element := p.stack[index]
+	p.stack = p.stack[:index]
+	return element
+}
+
+// empty returns true if and onl if the stack of nodes is empty.
+func (p *parser) emptyStack() bool {
+	return len(p.stack) == 0
+}
+
+// nextLexeme returns the next item from the input.
+func (p *parser) nextLexeme() lexeme {
+	if p.pos >= len(p.input) {
+		return eof
+	}
+	next := p.input[p.pos]
+	p.pos++
+	return next
+}
+
+// peek returns the next item from the input without consuming the item.
+func (p *parser) peek() lexeme {
+	if p.pos >= len(p.input) {
+		return eof
+	}
+	return p.input[p.pos]
+}
+
+func (p *parser) getNum() *node {
+	n := p.peek()
+	if n == "0" || n == "1" || n == "2" || n == "3" || n == "4" || n == "5" ||
+		n == "6" || n == "7" || n == "8" || n == "9" {
+		return &node{
+			lexeme:   n,
+			children: []*node{},
+		}
+	}
+	panic("digit expected")
+}
+
+func (p *parser) expression() {
+	p.tree = p.getNum()
+}
+
+func (p *parser) parse() *node {
+	if p.peek() == eof {
+		return nil
+	}
+	p.expression()
+	return p.tree
+}
---
+++ pkg/expr/parser_test.go
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2020 Go YAML Path Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package expr
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewNode(t *testing.T) {
+	cases := []struct {
+		name     string
+		lexemes  []lexeme
+		expected *node
+		focus    bool // if true, run only tests with focus set to true
+	}{
+		{
+			name:     "no lexemes",
+			lexemes:  []lexeme{},
+			expected: nil,
+		},
+		{
+			name:    "integer",
+			lexemes: []lexeme{"1"},
+			expected: &node{
+				lexeme:   "1",
+				children: []*node{},
+			},
+		},
+		// {
+		// 	name:    "sum",
+		// 	lexemes: []lexeme{"1", "+", "2"},
+		// 	expected: &node{
+		// 		lexeme: "+",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme:   "1",
+		// 				children: []*node{},
+		// 			},
+		// 			{
+		// 				lexeme:   "2",
+		// 				children: []*node{},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	name:    "multiplication",
+		// 	lexemes: []lexeme{"1", "*", "2"},
+		// 	expected: &node{
+		// 		lexeme: "*",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme:   "1",
+		// 				children: []*node{},
+		// 			},
+		// 			{
+		// 				lexeme:   "2",
+		// 				children: []*node{},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	name:    "1*2+3",
+		// 	lexemes: []lexeme{"1", "*", "2", "+", "3"},
+		// 	expected: &node{
+		// 		lexeme: "+",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme: "*",
+		// 				children: []*node{
+		// 					{
+		// 						lexeme:   "1",
+		// 						children: []*node{},
+		// 					},
+		// 					{
+		// 						lexeme:   "2",
+		// 						children: []*node{},
+		// 					},
+		// 				},
+		// 			},
+		// 			{
+		// 				lexeme:   "3",
+		// 				children: []*node{},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	name:    "1+2*3",
+		// 	lexemes: []lexeme{"1", "+", "2", "*", "3"},
+		// 	expected: &node{
+		// 		lexeme: "+",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme:   "1",
+		// 				children: []*node{},
+		// 			},
+		// 			{
+		// 				lexeme: "*",
+		// 				children: []*node{
+		// 					{
+		// 						lexeme:   "2",
+		// 						children: []*node{},
+		// 					},
+		// 					{
+		// 						lexeme:   "3",
+		// 						children: []*node{},
+		// 					},
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	name:    "1+2+3",
+		// 	lexemes: []lexeme{"1", "+", "2", "+", "3"},
+		// 	expected: &node{
+		// 		lexeme: "+",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme: "+",
+		// 				children: []*node{
+		// 					{
+		// 						lexeme:   "1",
+		// 						children: []*node{},
+		// 					},
+		// 					{
+		// 						lexeme:   "2",
+		// 						children: []*node{},
+		// 					},
+		// 				},
+		// 			},
+		// 			{
+		// 				lexeme:   "3",
+		// 				children: []*node{},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	name:    "1*2*3",
+		// 	lexemes: []lexeme{"1", "*", "2", "*", "3"},
+		// 	expected: &node{
+		// 		lexeme: "*",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme:   "1",
+		// 				children: []*node{},
+		// 			},
+		// 			{
+		// 				lexeme: "*",
+		// 				children: []*node{
+		// 					{
+		// 						lexeme:   "2",
+		// 						children: []*node{},
+		// 					},
+		// 					{
+		// 						lexeme:   "3",
+		// 						children: []*node{},
+		// 					},
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	name:    "simple bracket",
+		// 	lexemes: []lexeme{"(", "1", ")"},
+		// 	expected: &node{
+		// 		lexeme: "()",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme:   "1",
+		// 				children: []*node{},
+		// 			},
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	focus:   true,
+		// 	name:    "(1+2)*3",
+		// 	lexemes: []lexeme{"(", "1", "+", "2", ")", "*", "3"},
+		// 	expected: &node{
+		// 		lexeme: "*",
+		// 		children: []*node{
+		// 			{
+		// 				lexeme: "()",
+		// 				children: []*node{
+		// 					{
+		// 						lexeme: "+",
+		// 						children: []*node{
+		// 							{
+		// 								lexeme:   "1",
+		// 								children: []*node{},
+		// 							},
+		// 							{
+		// 								lexeme:   "2",
+		// 								children: []*node{},
+		// 							},
+		// 						},
+		// 					},
+		// 				},
+		// 			},
+		// 			{
+		// 				lexeme:   "3",
+		// 				children: []*node{},
+		// 			},
+		// 		},
+		// 	},
+		// },
+	}
+
+	focussed := false
+	for _, tc := range cases {
+		if tc.focus {
+			focussed = true
+			break
+		}
+	}
+
+	for _, tc := range cases {
+		if focussed && !tc.focus {
+			continue
+		}
+		t.Run(tc.name, func(t *testing.T) {
+			actual := newNode(tc.lexemes)
+			if focussed {
+				// sometimes easier to read this than a diff
+				fmt.Println("Expected:")
+				fmt.Println(tc.expected.String())
+				fmt.Println("Actual:")
+				fmt.Println(actual.String())
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+
+	if focussed {
+		t.Fatalf("testcase(s) still focussed")
+	}
+}
